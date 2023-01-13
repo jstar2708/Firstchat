6dcaaed2295eb8fe98565533b1cb160bcc97d63d
Jaideep Kumar <jaideepkumarsingh14@gmail.com>
2023-01-13 20:43:21 +0530

    Firstchat App started

---
+++ app/src/main/java/com/example/firstchat/auth/SignInActivity.kt
@@ -0,0 +1,12 @@
+package com.example.firstchat.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.firstchat.R
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
